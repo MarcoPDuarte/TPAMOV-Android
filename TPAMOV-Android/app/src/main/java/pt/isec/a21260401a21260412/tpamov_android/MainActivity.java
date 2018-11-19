9c5f5963b67ad2d9bd7701ac9c4a15187998b24e
Marco Duarte <marcosyn.11@gmail.com>
2018-11-19 18:28:07 +0000

    Começo

--- TPAMOV-Android/app/src/main/java/pt/isec/a21260401a21260412/tpamov_android/MainActivity.java
+++ TPAMOV-Android/app/src/main/java/pt/isec/a21260401a21260412/tpamov_android/MainActivity.java
@@ -2,13 +2,36 @@ package pt.isec.a21260401a21260412.tpamov_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    TextView title;
+    Button btnOP, btnTP, btnM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Teste
+
+        title = findViewById(R.id.MenuTitle);
+        btnOP = findViewById(R.id.btnOnePlayer);
+        btnTP = findViewById(R.id.btnTwoPlayers);
+        btnM = findViewById(R.id.btnMultiplayer);
+
     }
+
+    public void onOnePlayer(View v){
+
+    }
+
+    public void onTwoPlayers(View v){
+
+    }
+
+    public void onMultiplayer(View v){
+
+    }
+
 }
