479ff866c271b9c3cc15041ceddd41a6fe827062
Marco Duarte <marcosyn.11@gmail.com>
2018-11-19 23:11:26 +0000

    Interface do Profile
    Profile quase feito. Falta guardar a informação.

--- TPAMOV-Android/app/src/main/java/pt/isec/a21260401a21260412/tpamov_android/ManageProfileActivity.java
+++ TPAMOV-Android/app/src/main/java/pt/isec/a21260401a21260412/tpamov_android/ManageProfileActivity.java
@@ -1,13 +1,120 @@
 package pt.isec.a21260401a21260412.tpamov_android;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.lang.reflect.Method;
 
 public class ManageProfileActivity extends Activity {
+
+    TextView tvName;
+    EditText playerName;
+    Button btnTakePhoto;
+    ImageView imPhoto;
+    Uri fileUri;
+    String photoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_profile);
+
+        tvName = findViewById(R.id.textViewName);
+        playerName = findViewById(R.id.editTextGetName);
+        btnTakePhoto = findViewById(R.id.btnTakePhoto);
+        imPhoto = findViewById(R.id.ImageViewPhoto);
+        photoPath = "/sdcard/tmpfoto";
+    }
+
+    public void onTakePhoto(View v) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                    PackageManager.PERMISSION_GRANTED)
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            try {
+                Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
+                m.invoke(null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = Uri.fromFile(new File(photoPath));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(intent, 20);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 20) {
+            setPic(imPhoto, photoPath);
+        }
+    }
+
+    private void setPic(ImageView ImageView, String mCurrentPhotoPath) { // Get the dimensions of the View
+        int targetW = ImageView.getWidth();
+        int targetH = ImageView.getHeight();
+
+        if (targetW == 0 || targetH == 0)
+            return;
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions); // existem outros. Ex: decodeStream
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        ImageView.setImageBitmap(bitmap); //em alternativa retornar apenas o Bitmap
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.add_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.addMenu){
+            //Adicionar info ao Player /***/
+            finish();
+        }
+
+        return true;
     }
 }
+
