c608281694eabf0df450319751ce8e549737cf70
Marco Lopes <marcoi.lopes.98@gmail.com>
2018-11-19 19:19:12 +0000

    Atividade 2

---
+++ TPAMOV-Android/app/src/main/java/pt/isec/a21260401a21260412/tpamov_android/GameActivity.java
@@ -0,0 +1,14 @@
+package pt.isec.a21260401a21260412.tpamov_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+}
